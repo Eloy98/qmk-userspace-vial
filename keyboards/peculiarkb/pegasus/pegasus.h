944f9dfb5e5bbd7c7a2e246a8786f2a072be09bc
Eloy98 <eloy1172240653@gmail.com>
2024-11-01 01:12:00 +0800

    fix key matirx

--- keyboards/peculiarkb/pegasus/pegasus.h
+++ keyboards/peculiarkb/pegasus/pegasus.h
@@ -34,24 +34,24 @@
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-    K30, K31,                     K36,           K39,      K3B \
+    K30, K31,                     K35,           K38,      K3A \
 ) \
 { \
     { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B }, \
     { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B }, \
     { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B }, \
-    { K30, K31, XXX, XXX, XXX, XXX, K36, XXX, XXX, K39, XXX, K3B } \
+    { K30, K31, XXX, XXX, XXX, K35, XXX, XXX, K38, XXX, K3A, XXX } \
 }
 
 #define LAYOUT_split( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-    K30, K31, K32,      K34,       K36,     K38, K39,      K3B \
+    K30, K31, K32,      K34, K35,      K37, K38,      K3A       \
 ) \
 { \
     { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B }, \
     { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B }, \
     { K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B }, \
-    { K30, K31, K32, XXX, K34, XXX, K36, XXX, K38, K39, XXX, K3B } \
+    { K30, K31, K32, XXX, K34, K35, XXX, K37, K38, XXX, K3A, XXX } \
 }
