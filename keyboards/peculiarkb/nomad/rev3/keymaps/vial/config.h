2ec36c78ace6f070d51cf568cfe70c108cd212ae
Eloy <52245532+Eloy98@users.noreply.github.com>
2025-08-24 21:56:47 +0800

    add nomad

---
+++ keyboards/peculiarkb/nomad/rev3/keymaps/vial/config.h
@@ -0,0 +1,25 @@
+/* Copyright 2024 Andreas Tacke
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define LSPO_KEYS KC_LSFT, KC_LSFT, KC_8
+#define RSPC_KEYS KC_RSFT, KC_RSFT, KC_9
+
+#pragma once
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#define VIAL_KEYBOARD_UID {0x26, 0x71, 0x40, 0xC1, 0x5B, 0xC5, 0x7F, 0xDE}
